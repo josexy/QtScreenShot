7462d42532abe5b94faf35664ffbbb242b79e4a5
josexy <josephxrays@163.com>
2020-10-08 19:58:45 +0800

    drawer

--- drawer/arrowdrawer.cpp
+++ drawer/arrowdrawer.cpp
@@ -10,7 +10,19 @@ ArrowDrawer::ArrowDrawer(const QPoint &from, const QPoint &to,
 
     if(from.x()==-1||from.y()==-1||to.x()==-1||to.y()==-1) return;
     QLineF line(from,to);
-    if(shapeType==ShapeType::TriArrow){
+
+    if(shapeType==ShapeType::LineArrow){
+        double angle = std::atan2(-line.dy(), line.dx());
+        qreal arrowSize = lineSize;
+        QPointF arrowP1 = line.p2() - QPointF(sin(angle + M_PI / 3) * arrowSize, cos(angle + M_PI / 3) * arrowSize);
+        QPointF arrowP2 = line.p2() - QPointF(sin(angle + M_PI - M_PI / 3) * arrowSize, cos(angle + M_PI - M_PI / 3) * arrowSize);
+        QPolygonF arrowHead;
+        arrowHead  << line.p2() << arrowP2 << arrowP1;
+        p.drawLine(line);
+        p.drawLine(to, arrowP1);
+        p.drawLine(to, arrowP2);
+
+    }else if(shapeType==ShapeType::TriArrow){
         QPainterPath pp;
         QLineF l1=line.unitVector();
 
