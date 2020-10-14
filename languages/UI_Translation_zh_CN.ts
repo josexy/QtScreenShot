9ac72e59fb7468b681da872b87ef2ebfc5afc9a0
josexy <josephxrays@163.com>
2020-10-14 21:13:37 +0800

    UI_Translation_zh_CN.ts

--- languages/UI_Translation_zh_CN.ts
+++ languages/UI_Translation_zh_CN.ts
@@ -1,24 +1,8 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="zh_CN">
+<TS version="2.1" language="zh_CN">
 <context>
     <name>ArrowTool</name>
-    <message>
-        <source>arrow1</source>
-        <translation>箭头1</translation>
-    </message>
-    <message>
-        <source>tri arrow</source>
-        <translation>箭头1</translation>
-    </message>
-    <message>
-        <source>line arrow</source>
-        <translation>箭头2</translation>
-    </message>
-    <message>
-        <source>sharp arrow</source>
-        <translation>箭头3</translation>
-    </message>
     <message>
         <location filename="../tool/arrowtool.cpp" line="21"/>
         <source>Triangle Arrow</source>
@@ -120,10 +104,6 @@
         <translation>语言(需重启):</translation>
     </message>
     <message>
-        <source>Start on boot:</source>
-        <translation>开机时启动:</translation>
-    </message>
-    <message>
         <location filename="../ui/settingswindow.ui" line="75"/>
         <source>Start on boot(Windows):</source>
         <translation>开机时启动(Windows):</translation>
@@ -185,155 +165,215 @@
     </message>
     <message>
         <location filename="../ui/settingswindow.ui" line="281"/>
+        <source>Upload</source>
+        <translation>上传</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="328"/>
+        <source>Proxy Setting</source>
+        <translation>代理设置</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="352"/>
+        <source>SOCKS5</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="379"/>
+        <source>IP: </source>
+        <translation>IP地址:</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="386"/>
+        <source>127.0.0.1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="399"/>
+        <source>Port: </source>
+        <translation>端口:</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="406"/>
+        <source>8888</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="289"/>
+        <source>Upload image to smms</source>
+        <translation>上传图片到SMMS</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="296"/>
+        <source>Enable proxy</source>
+        <translation>开启代理</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="309"/>
+        <source>Smms Authorization: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="336"/>
+        <source>Type: </source>
+        <translation>代理类型:</translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="347"/>
+        <source>HTTP</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/settingswindow.ui" line="421"/>
         <source>Hotkey</source>
         <translation>热键</translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="291"/>
+        <location filename="../ui/settingswindow.ui" line="431"/>
         <source>Capture desktop:</source>
         <translation>截取桌面:</translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="298"/>
+        <location filename="../ui/settingswindow.ui" line="438"/>
         <source>Ctrl+Shift+A</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="309"/>
+        <location filename="../ui/settingswindow.ui" line="449"/>
         <source>Capture desktop and copy:</source>
         <translation>截取桌面并复制:</translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="316"/>
+        <location filename="../ui/settingswindow.ui" line="456"/>
         <source>Ctrl+Shift+C</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="327"/>
+        <location filename="../ui/settingswindow.ui" line="467"/>
         <source>General capture:</source>
         <translation>常规截屏:</translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="334"/>
+        <location filename="../ui/settingswindow.ui" line="474"/>
         <source>F1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="345"/>
+        <location filename="../ui/settingswindow.ui" line="485"/>
         <source>Setting dialog:</source>
         <translation>设置对话框:</translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="352"/>
+        <location filename="../ui/settingswindow.ui" line="492"/>
         <source>Alt+Shift+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="363"/>
+        <location filename="../ui/settingswindow.ui" line="503"/>
         <source>Quit application:</source>
         <translation>退出程序:</translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="370"/>
+        <location filename="../ui/settingswindow.ui" line="510"/>
         <source>Alt+Shift+Q</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="382"/>
+        <location filename="../ui/settingswindow.ui" line="522"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="397"/>
+        <location filename="../ui/settingswindow.ui" line="537"/>
         <source>About Qt</source>
         <translation>关于Qt</translation>
     </message>
     <message>
-        <location filename="../ui/settingswindow.ui" line="421"/>
+        <location filename="../ui/settingswindow.ui" line="561"/>
         <source>Save configuration</source>
         <translation>保存设置</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="71"/>
-        <location filename="../settingswindow.cpp" line="78"/>
-        <location filename="../settingswindow.cpp" line="85"/>
-        <location filename="../settingswindow.cpp" line="93"/>
-        <location filename="../settingswindow.cpp" line="100"/>
+        <location filename="../settingswindow.cpp" line="72"/>
+        <location filename="../settingswindow.cpp" line="79"/>
+        <location filename="../settingswindow.cpp" line="86"/>
+        <location filename="../settingswindow.cpp" line="94"/>
+        <location filename="../settingswindow.cpp" line="101"/>
         <source>Recording keystrokes...</source>
         <translation>正在记录按键...</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="106"/>
+        <location filename="../settingswindow.cpp" line="107"/>
         <source>Right click to select the dot color</source>
         <translation>右键选择边角颜色</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="107"/>
+        <location filename="../settingswindow.cpp" line="108"/>
         <source>Right click to select the border color</source>
         <translation>右键选择边框颜色</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="108"/>
+        <location filename="../settingswindow.cpp" line="109"/>
         <source>Right click to select UI color</source>
         <translation>右键选择界面颜色</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="126"/>
+        <location filename="../settingswindow.cpp" line="127"/>
         <source>Shortcut</source>
         <translation>快捷键</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="126"/>
+        <location filename="../settingswindow.cpp" line="127"/>
         <source>Description</source>
         <translation>描述</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="133"/>
+        <location filename="../settingswindow.cpp" line="134"/>
         <source>Move capture region</source>
         <translation>调整捕获窗口</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="136"/>
+        <location filename="../settingswindow.cpp" line="137"/>
         <source>Move pixel panel</source>
         <translation>移动选择</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="139"/>
+        <location filename="../settingswindow.cpp" line="140"/>
         <source>Close capture window</source>
         <translation>关闭捕获窗口</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="142"/>
+        <location filename="../settingswindow.cpp" line="143"/>
         <source>Capture Desktop</source>
         <translation>截取桌面</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="145"/>
+        <location filename="../settingswindow.cpp" line="146"/>
         <source>Capture Desktop and copy</source>
         <translation>截取桌面并复制</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="148"/>
+        <location filename="../settingswindow.cpp" line="149"/>
         <source>General capture</source>
         <translation>常规截屏</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="151"/>
+        <location filename="../settingswindow.cpp" line="152"/>
         <source>Settings dialog</source>
         <translation>设置对话框</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="154"/>
+        <location filename="../settingswindow.cpp" line="155"/>
         <source>Quit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="338"/>
+        <location filename="../settingswindow.cpp" line="364"/>
         <source>Information</source>
         <translation>消息</translation>
     </message>
     <message>
-        <location filename="../settingswindow.cpp" line="338"/>
+        <location filename="../settingswindow.cpp" line="364"/>
         <source>Settings saved successfully!</source>
         <translation>设置保存成功!</translation>
     </message>
@@ -374,48 +414,48 @@
 <context>
     <name>TMain</name>
     <message>
-        <location filename="../tmain.cpp" line="39"/>
+        <location filename="../tmain.cpp" line="40"/>
         <source>Qt screenshot tool</source>
         <translation>Qt 截屏工具</translation>
     </message>
     <message>
-        <location filename="../tmain.cpp" line="49"/>
+        <location filename="../tmain.cpp" line="50"/>
         <source>Capture desktop</source>
         <translation>截取桌面</translation>
     </message>
     <message>
-        <location filename="../tmain.cpp" line="53"/>
+        <location filename="../tmain.cpp" line="54"/>
         <source>Capture desktop and copy</source>
         <translation>截取桌面并复制</translation>
     </message>
     <message>
-        <location filename="../tmain.cpp" line="57"/>
+        <location filename="../tmain.cpp" line="58"/>
         <source>General capture</source>
         <translation>常规截屏</translation>
     </message>
     <message>
-        <location filename="../tmain.cpp" line="61"/>
+        <location filename="../tmain.cpp" line="62"/>
         <source>Settings dialog</source>
         <translation>设置对话框</translation>
     </message>
     <message>
-        <location filename="../tmain.cpp" line="65"/>
+        <location filename="../tmain.cpp" line="66"/>
         <source>Quit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../tmain.cpp" line="198"/>
-        <location filename="../tmain.cpp" line="205"/>
+        <location filename="../tmain.cpp" line="214"/>
+        <location filename="../tmain.cpp" line="225"/>
         <source>QtScreenShot notification</source>
         <translation>QtScreenShot 通知</translation>
     </message>
     <message>
-        <location filename="../tmain.cpp" line="198"/>
+        <location filename="../tmain.cpp" line="214"/>
         <source>The image has been saved to a file</source>
         <translation>图像已保存到文件</translation>
     </message>
     <message>
-        <location filename="../tmain.cpp" line="205"/>
+        <location filename="../tmain.cpp" line="225"/>
         <source>The image has been saved to the clipboard</source>
         <translation>图像已保存到剪切板</translation>
     </message>
@@ -456,83 +496,83 @@
 <context>
     <name>ToolWidget</name>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="24"/>
+        <location filename="../widget/toolwidget.cpp" line="25"/>
         <source>shape tool</source>
         <translation>图形</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="25"/>
+        <location filename="../widget/toolwidget.cpp" line="26"/>
         <source>pen tool</source>
         <translation>画笔</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="26"/>
+        <location filename="../widget/toolwidget.cpp" line="27"/>
         <source>text tool</source>
         <translation>文本</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="27"/>
+        <location filename="../widget/toolwidget.cpp" line="28"/>
         <source>arrow tool</source>
         <translation>箭头</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="28"/>
+        <location filename="../widget/toolwidget.cpp" line="29"/>
         <source>blur</source>
         <translation>模糊</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="29"/>
+        <location filename="../widget/toolwidget.cpp" line="30"/>
         <source>Brush tool</source>
         <translation>画刷</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="30"/>
+        <location filename="../widget/toolwidget.cpp" line="31"/>
         <source>erase tool</source>
         <translation>橡皮擦</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="31"/>
+        <location filename="../widget/toolwidget.cpp" line="32"/>
         <source>undo</source>
         <translation>撤销</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="32"/>
+        <location filename="../widget/toolwidget.cpp" line="33"/>
         <source>redo</source>
         <translation>重做</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="33"/>
+        <location filename="../widget/toolwidget.cpp" line="34"/>
         <source>close</source>
         <translation>关闭</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="34"/>
+        <location filename="../widget/toolwidget.cpp" line="35"/>
         <source>pin tool</source>
         <translation>贴图</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="35"/>
+        <location filename="../widget/toolwidget.cpp" line="36"/>
         <source>save to file</source>
         <translation>保存到文件</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="36"/>
+        <location filename="../widget/toolwidget.cpp" line="37"/>
         <source>copy to clipboard</source>
         <translation>复制到剪贴板</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="122"/>
-        <location filename="../widget/toolwidget.cpp" line="135"/>
+        <location filename="../widget/toolwidget.cpp" line="124"/>
+        <location filename="../widget/toolwidget.cpp" line="142"/>
         <source>QtScreenShot notification</source>
         <translation>QtScreenShot 通知</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="122"/>
+        <location filename="../widget/toolwidget.cpp" line="124"/>
         <source>The image has been saved to a file</source>
         <translation>图像已保存到文件</translation>
     </message>
     <message>
-        <location filename="../widget/toolwidget.cpp" line="135"/>
+        <location filename="../widget/toolwidget.cpp" line="142"/>
         <source>The image has been saved to the clipboard</source>
         <translation>图像已保存到剪切板</translation>
     </message>
